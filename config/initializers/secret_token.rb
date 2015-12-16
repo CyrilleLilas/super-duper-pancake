b396a3109075df8a5503ac2ccb18649559ab966b
Cyrille <pancakos@gmail.com>
2015-12-16 20:09:31 +0100

    created the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SuperDuperPancake::Application.config.secret_key_base = '337b1ec5f66043e185e1a2620c817256ead2b793575cf9f74ab9fe9dc2e19a6ebab1f6726b70493b4f35833bb1c55486e46bb0fa11389b89cf5f3fb45e8fc7f7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SuperDuperPancake::Application.config.session_store :cookie_store, key: '_super-duper-pancake_session'
