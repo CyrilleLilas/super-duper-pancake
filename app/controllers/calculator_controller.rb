97a01073fe5aa8fc89e16e30bc611a51edfdcce9
Cyrille <pancakos@gmail.com>
2015-12-17 21:11:38 +0100

    tests pass floating point issue remaining

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -2,36 +2,52 @@ class CalculatorController < ApplicationController
 	before_action :safe_params
 
 	def do_the_maths
-		logger.debug "PARAMS #{@a}"
-		a = params[:a].to_f
-		b = params[:b].to_f
+		a = params[:A].to_f
+		b = params[:B].to_f
+			logger.debug "A  #{a}"
 
-		if params[:op] === 'sum'
+		if params[:op] === 'add'
 			@result = a + b
-		elsif params[:op] === '-'
+		elsif params[:op] === 'sub'
 			@result = a - b
-		elsif params[:op] === '*'
+		elsif params[:op] === 'mul'
 			@result = a * b
-		elsif params[:op] === '/'
+		elsif params[:op] === 'div'
 			@result = a / b
-		else
-			render :nothing => true, :status => 400
 		end 
 
 		if !@result.nil?
-			render text: @result, content_type: 'text/plain'
+			if params[:output] === 'json'
+				render text: {result: @result}.to_json, content_type: 'application/json'
+			else 
+				render text: @result, content_type: 'text/plain'
+			end
 		end
 	end
 
 	private
 		def safe_params
-			params.permit(:a, :b, :op)
+			params.permit(:A, :B, :op, :output)
+			params.each {|k,v| v.downcase!}
 			nb_regex = /^-*\d+\.*\d*$/
-			if nb_regex =~ params[:a] && nb_regex =~ params[:b]
-			else
-				render :nothing => true, :status => 400
+			op_regex = /^(add|sub|mul|div)$/
+			if params[:A].nil?
+				bad_request "missing operand A"
+			elsif params[:B].nil?
+				bad_request "missing operand B"
+			elsif params[:op].nil?
+				bad_request "missing operator"
+			elsif !(op_regex =~ params[:op])
+				bad_request "unknown operator"
+			elsif !(nb_regex =~ params[:A])
+				bad_request "operand A not a number"
+			elsif !(nb_regex =~ params[:B])
+				bad_request "operand B not a number"
 			end
-			logger.debug "IS_MATCH #{nb_regex =~ params[:a]}"
-			# return { :a => params[:]}
 		end
+
+		def bad_request(msg)
+			render text: msg, content_type: 'text/plain', :status => 400
+		end
+			
 end
--- test/controllers/calculator_controller_test.rb
+++ test/controllers/calculator_controller_test.rb
@@ -4,4 +4,112 @@ class CalculatorControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+	test "should sum" do
+	  get(:do_the_maths, {A: "5", B: "6", op: "add"})
+	  assert_equal "11.0", @response.body
+	  # Rails.logger.debug "RESPONSE #{@response.body.body.to_s}"
+	  assert_response 200
+	end
+
+	test "should substract" do
+	  get(:do_the_maths, {A: "5", B: "6", op: "sub"})
+	  assert_equal "-1.0", @response.body
+	  assert_response 200
+	end
+
+	test "should multiply" do
+	  get(:do_the_maths, {A: "5", B: "6", op: "mul"})
+	  assert_equal "30.0", @response.body
+	  assert_response 200
+	end
+
+	test "should divide" do
+	  get(:do_the_maths, {A: "5", B: "2", op: "div"})
+	  assert_equal "2.5", @response.body
+	  assert_response 200
+	end
+
+	test "should accept floats" do
+	  get(:do_the_maths, {A: "5.48", B: "6.23", op: "mul"})
+	  assert_equal "34.1404", @response.body
+	  assert_response 200
+	end
+
+	test "should accept one negative number" do
+	  get(:do_the_maths, {A: "-5.48", B: "6.23", op: "mul"})
+	  assert_equal "-34.1404", @response.body
+	  assert_response 200
+	end
+
+	test "should accept two negatives number" do
+	  get(:do_the_maths, {A: "-5.48", B: "-6.23", op: "mul"})
+	  assert_equal "34.1404", @response.body
+	  assert_response 200
+	end
+
+	test "should accept big numbers" do
+	  get(:do_the_maths, {A: "111111111111111111111111111111111111111111111111111111111", B: "2", op: "add"})
+	  assert_equal "111111111111111111111111111111111111111111111111111111113", @response.body
+	  assert_response 200
+	end
+
+	test "operand B is mandatory" do
+	  get(:do_the_maths, {A: "5", op: "div"})
+	  assert_equal "missing operand B", @response.body
+	  assert_response 400
+	end
+
+	test "operand A is mandatory" do
+	  get(:do_the_maths, {B: "0", op: "div"})
+	  assert_equal "missing operand A", @response.body
+	  assert_response 400
+	end
+
+	test "should not accept missing operator" do
+	  get(:do_the_maths, {A: "5", B: "71"})
+	  assert_equal "missing operator", @response.body
+	  assert_response 400
+	end
+
+	test "should not accept unknown operator" do
+	  get(:do_the_maths, {A: "5", B: "71", op: "lollipop"})
+	  assert_equal "unknown operator", @response.body
+	  assert_response 400
+	end
+
+	test "divide by zero" do
+	  get(:do_the_maths, {A: "5", B: "0", op: "div"})
+	  assert_equal "Infinity", @response.body
+	  assert_response 200
+	end
+
+	test "operand A should represent a number" do
+	  get(:do_the_maths, {A: "5sfdguisg", B: "0", op: "add"})
+	  assert_equal "operand A not a number", @response.body
+	  assert_response 400
+	end
+
+	test "operand B should represent a number" do
+	  get(:do_the_maths, {A: "45", B: "five", op: "add"})
+	  assert_equal "operand B not a number", @response.body
+	  assert_response 400
+	end
+
+	test "should render json when asked" do
+	  get(:do_the_maths, {A: "-5.48", B: "-6.23", op: "mul", output: "json"})
+	  assert_equal({result: 34.1404}.to_json, @response.body)
+	  assert_response 200
+	end
+
+	test "should accept uppercase operators" do
+	  get(:do_the_maths, {A: "-5.48", B: "-6.23", op: "MUL", output: "json"})
+	  assert_equal({result: 34.1404}.to_json, @response.body)
+	  assert_response 200
+	end
+
+	test "parameters values should be case insentive" do
+	  get(:do_the_maths, {A: "-5.48", B: "-6.23", op: "MUL", output: "jSOn"})
+	  assert_equal({result: 34.1404}.to_json, @response.body)
+	  assert_response 200
+	end
 end
